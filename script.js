1b196e4d351089cbd9dcf0bf3f60867498a56596
awrdj <awrdjmusic@gmail.com>
2025-05-05 23:38:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1847,7 +1847,7 @@ $(document).ready(function() {
 
     // Fetch suggestions from Amazon API
     function getSuggestions(queryFirst, queryLast, marketplace, apiType = 'Generic') {
-        const departmentQuery = 'stripbooks'; // Hardcoded 'aps' (All Departments) like extension
+        const departmentQuery = 'fashion'; // Hardcoded 'aps' (All Departments) like extension
         const params = new URLSearchParams({
             'site-variant': 'desktop',
             'mid': marketplace.market,

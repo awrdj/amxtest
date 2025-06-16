f727ff4e39b5bf9c507a0e511f9fdd229001a7d9
awrdj <awrdjmusic@gmail.com>
2025-06-16 14:02:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ productTypeOverrides: (Object) - New - Contains nested objects where keys are pr
             { value: 'last90-fashion-com', text: 'Last 90 Days Fashion', 
               settings: { timeFilter: 'timeFilter90Days', sortOrder: 'custom', department: 'fashion', category: '7141123011', productType: 'custom', suppressDefaultProductTypeKeywords: false} },
             { value: 'most-purchased-com', text: 'Most Purchased Fashion Novelty', 
-              settings: { sortOrder: 'most-purchased-rank', department: 'fashion-novelty', productType: 'custom', suppressDefaultProductTypeKeywords: false} },
+              settings: { sortOrder: 'most-purchased-rank', department: 'fashion', productType: 'custom', suppressDefaultProductTypeKeywords: false} },
             { value: 'the-trends-com', text: 'Trends Fashion Novelty', 
               settings: { sortOrder: 'date-desc-rank', department: 'fashion-novelty', productType: 'custom', suppressDefaultProductTypeKeywords: false} },
             { value: 'archive-view-com', text: 'Archive Fashion Novelty', 
